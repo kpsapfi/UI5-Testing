7be603de4822a0eaabbf95e4bfb898dec9a76bc6
kalen <kalen.peredo@sapfioneer.com>
2024-10-28 10:54:24 -0600

    Added unit test for new module: flagging posts Added unit test that tests for button state pased on the Flagged property in model. Checks for formatting and parsing conversions. No logic has been implemented yet, so the tests will fail at this time

---
+++ webapp/model/FlaggedType.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/model/SimpleType"
+], function (SimpleType) {
+    "user strict";
+    return SimpleType.extend("sap.ui.demo.bulletinboard.model.FlaggedType", {
+        // Formats a model value to be displayed in UI
+        formatValue: function() {
+        },
+        // Parses UI value to be stored in model
+        parseValue: function() {
+        },
+        // Checks for dispalying validation errors
+        validateValue: function() {
+        }
+    });
+});
